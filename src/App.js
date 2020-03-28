8f518be7c7c9ff96dd9e40380e5b474c4490936e
Andres Lemus <aclemusm@unal.edu.co>
2020-03-27 21:34:45 -0500

    Configure cra

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styled from 'styled-components';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBullhorn } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBullhorn);
+
+class App extends Component {
+  render() {
+    const Title = styled.h1`
+      font-family: sans-serif;
+    `;
+
+    return (
+      <Title>
+        <FontAwesomeIcon icon="bullhorn" /> News Room
+      </Title>
+    );
+  }
 }
 
 export default App;
